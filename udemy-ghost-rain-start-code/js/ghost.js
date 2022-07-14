7fcc48401d7a7ce2965e4e00614d454b6bf97c2d
poylib <poy_lib@naver.com>
2022-07-14 23:38:59 +0900

    add_ghost
    유령생성

---
+++ udemy-ghost-rain-start-code/js/ghost.js
@@ -0,0 +1,10 @@
+let ghostElement = document.createElement('div');
+
+ghostElement.style.position = 'absolute';
+ghostElement.style.top = '0';
+ghostElement.style.left = '50%';
+ghostElement.style.width = `${GHOST_WIDTH}px`
+ghostElement.style.height = `${GHOST_HEIGHT}px`
+ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
+
+bgElement.appendChild(ghostElement);
