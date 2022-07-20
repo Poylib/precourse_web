e84f70e081861e245ff63898b3ac68b4df30df82
poylib <poy_lib@naver.com>
2022-07-20 10:18:12 +0900

    finish(basic tool)
    기본 기능제공까지 끝
    
    추가적인 기능들 넣을 예정

--- udemy-ghost-rain-start-code/js/ghost.js
+++ udemy-ghost-rain-start-code/js/ghost.js
@@ -1,34 +1,60 @@
-let topGhost = 0;
-let ghostElement = document.createElement('div');
+
 
 function createGhost() {
+  let topGhost = 0;
+  let ghostElement = document.createElement('div');  
   let randomLeft = randomRange(0,BG_WIDTH-GHOST_WIDTH);
   ghostElement.style.top = `${topGhost}px`;
   ghostElement.style.position = 'absolute';
-  ghostElement.style.left = randomLeft + 'px';
+  ghostElement.style.left = `${randomLeft}px`;
   ghostElement.style.width = `${GHOST_WIDTH}px`
   ghostElement.style.height = `${GHOST_HEIGHT}px`
   ghostElement.style.background = 'url("./images/ghost.png") no-repeat';
   
   bgElement.appendChild(ghostElement);
 
-  moveGhost() //유령비 내리게 하는 함수
+  moveGhost(topGhost,ghostElement); //유령비 내리게 하는 함수
 }
 
 function randomRange(min,max) {
   return Math.floor(Math.random()*(max+1-min));
 } //유령 위치 랜덤 생성
 
-function moveGhost() {
-  let requestAni;
-  topGhost++;
-  ghostElement.style.top = `${topGhost}px`;
-  requestAni = requestAnimationFrame(moveGhost);  //움직이게 하는 함수
+function moveGhost(top,el) {
+  top++;
   
-  if(topGhost > BG_HEIGHT -GHOST_HEIGHT) {
-    cancelAnimationFrame(requestAni)  //멈추게 하는 함수
-    ghostElement.remove();
+  if(top > BG_HEIGHT-(HERO_HEIGHT+GHOST_HEIGHT)) {
+    const ghostLeft = Number(el.style.left.split('px')[0]);
+    const heroLeft = Number(heroElement.style.left.split('px')[0]);
+    if(ghostLeft<heroLeft && heroLeft < ghostLeft+GHOST_WIDTH) {
+      die(el);
+      return;
+    }
+    if(top > BG_HEIGHT - GHOST_HEIGHT) {
+      remove(el);
+      return;
+    }
   }
+  ////////////////// 유령비를 내리게 하는 부분
+  el.style.top = `${top}px`;
+  let requestAni;
+  requestAni = window.requestAnimationFrame(function(){
+    moveGhost(top,el)
+  });  //움직이게 하는 함수
+  //////////////////
 }
 
-createGhost();
+function remove(e) {
+  e.remove();
+}
+
+function die(el) {
+  el.style.backgroundPosition = '-45px';
+
+  const sound = new Audio('./audio/dying.wav');
+  sound.play();
+
+  setTimeout(()=> {
+    remove(el)
+  },3000);
+}
--- udemy-ghost-rain-start-code/js/hero.js
+++ udemy-ghost-rain-start-code/js/hero.js
@@ -1,33 +1,10 @@
-document.addEventListener('keydown', function(e){
-  // 화면에서 키 누르면 발생할 동작
-  // 왼쪽 37, 오른쪽 39
-  
-  let keyCode = e.keyCode;
-  let heroLeft = getComputedStyle(heroElement).left;
-  //heroLeft.split('px')[0] << string이라 숫자로 바꿔줘야 함
-  let heroLeftPx = Number(heroLeft.split('px')[0])
+function setLeft(left) {
+  const currentLeft = Number(getComputedStyle(heroElement).left.split('px')[0]);
+  // getComputedStyle은 읽기 형식으로 단순히 값을 가져오기 위해 사용한 듯 하다.
+  // heroElement.style.left 를 사용해 값을 저장해도 에러는 없다.
+  const newLeft = currentLeft +left;
 
-  if (
-    // 캐릭터 크기만큼  +- 해줘야 한다. 아니면 끝에 멈추고 캐릭터가 조금 튀어나옴
-    // 아래 if문이 움직임을 인식하는 명령이므로 이 if 문이 밑으로 내려가면, 벽에 걸릴일 없이 계속 이동됨
-    (heroLeftPx -10 < 0 && keyCode === 37) ||
-    (heroLeftPx +10  > BG_WIDTH - HERO_WIDTH && keyCode === 39)
-  ) {
-    // return..  함수를 종료
-    return;
-  }
+  if(newLeft>BG_WIDTH - HERO_WIDTH || newLeft < 0) return;
 
-  if(keyCode === 37) {
-    heroElement.style.left = `${heroLeftPx -10}px`;
-    heroElement.className = 'left';
-  } else if(keyCode === 39) {
-    heroElement.style.left = `${heroLeftPx +10}px`;
-    heroElement.className = 'right';
-  }
-  // heroElement.style.left = '30px'
-
-});
-
-document.addEventListener('keyup',function(){
-  heroElement.className = 'stop';
-})
+  heroElement.style.left = `${newLeft}px`
+}
